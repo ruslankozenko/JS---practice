af3bafe91602a6695a627a9d757f0d8c4f177c62
ruslankozenko <kozenko.r@seznam.cz>
2025-10-22 19:53:04 +0200

    this

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-'use strict';
+// 'use strict';
 
 // Practice - (prototype)
 
-const person = new Object({
+// const person = new Object({
+//     name: 'Ruslan',
+//     age: 51,
+//     greet: function() {
+//         console.log('Greet!')
+//     }
+// })
+
+// Object.prototype.sayHello = function() {
+//     console.log('Hello!')
+// }
+ 
+// const lena = Object.create(person)
+// lena.name = 'Olena'
+
+// const str = new String('I am string')
+
+// console.log(str.sayHello())
+
+
+// Practice - (this)
+
+function hello() {
+    console.log('Hello', this)
+}
+
+const person = {
     name: 'Ruslan',
     age: 51,
-    greet: function() {
-        console.log('Greet!')
-    }
-})
-
-Object.prototype.sayHello = function() {
-    console.log('Hello!')
+    sayHello: hello
 }
- 
-const lena = Object.create(person)
-lena.name = 'Olena'
 
-const str = new String('I am string')
-
-console.log(str.sayHello())
+console.log(person.sayHello())
